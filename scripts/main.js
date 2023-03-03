44d5e4eaa047e96f0fd3ec1d935286ba14065228
emiliamassing <emiliamassings@gmail.com>
2023-03-03 19:26:59 +0100

    small changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,37 @@
+const inputUsername = document.querySelector('#username');
+const inputPassword = document.querySelector('#password');
+const loginButton = document.querySelector('#loginButton');
 
 
-function updateUsers(){
-    fetch('http://localhost:3000/users')
+fetch('http://localhost:3000/users')
     .then(res => res.json())
     .then(data => {
         console.log(data);
+});
+
+loginButton.addEventListener('click', tryToLogin);
+
+function tryToLogin() {
+    let logInUser = {
+        username: inputUsername.value,
+        password: inputPassword.value
+    };
+
+    fetch('http://localhost:3000/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(logInUser)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+
+        if(data.username){
+            console.log('Välkommen');
+        } else {
+            console.log('något gick fel');
+        }
     });
 };
-
-updateUsers();
